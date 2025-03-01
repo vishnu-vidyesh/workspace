132a055dc09d8a8e23a876c38f7d6f8b7ee9b4a7
vishnu-vidyesh <vishnuvidyesh@gmail.com>
2025-03-01 10:39:45 +0530

    added download clean ups to tool install flow

--- tools/install.sh
+++ tools/install.sh
@@ -74,6 +74,8 @@ echo "export PATH=$TOOLCHAIN_DIR/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-
 
 # Reload .bashrc to apply changes
 source ~/.bashrc
-
+echo "Clean Up"
+echo "Removing riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14.tar.gz"
+rm $TOOLCHAIN_DIR/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14.tar.gz
 echo "Done with toolchain installation!"
 
